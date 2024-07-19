685e16f8f11c16aeea356e26dc0525e32df87610
Rediet <red8alx@gmail.com>
2024-07-19 16:31:36 +0300

    fns_and_dsa

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            result = num1 + num2
+            return result
+        case "subtract":
+            result = num1 - num2
+            return result
+        case "multiply":
+            result = num1 * num2
+            return result
+        case "divide":
+            if num2 == 0:
+                errorMsg = "Cannot divide by zero"
+                return errorMsg
+            else:
+                result = num1 / num2
+                return result
+        case _:
+            print("Wrong type of operation")
