3ff0a5a5103fbcd37357a28e5580e85abe878f59
Rediet <red8alx@gmail.com>
2024-07-21 15:05:39 +0300

    fns_and_dsa

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -9,17 +9,17 @@ def convert_to_fahrenheit(celsius):
     global CELSIUS_TO_FAHRENHEIT_FACTOR
     fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return fahrenheit
-temp = input("Enter the temperature to convert: ")
-if temp.isnumeric():
-    temp = float(temp)
-    choice = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
-    match choice:
-        case 'C':
-            converted_temp = convert_to_fahrenheit(temp)
-            print(temp,"\u2103 is",converted_temp,"\u2109")
-        case 'F':
-            converted_temp = convert_to_celsius(temp)
-            print(temp,"\u2109 is",converted_temp,"\u2103")
+temperature = input("Enter the temperature to convert: ")
+if temperature.isnumeric():
+    temperature = float(temperature)
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().lower()
+    match unit:
+        case 'c':
+            converted_temp = convert_to_fahrenheit(temperature)
+            print(temperature,"\u2103 is",converted_temp,"\u2109")
+        case 'f':
+            converted_temp = convert_to_celsius(temperature)
+            print(temperature,"\u2109 is",converted_temp,"\u2103")
         case _:
             print("Invalid input. Please enter C or F to indicate which scale is used.")
 else:
