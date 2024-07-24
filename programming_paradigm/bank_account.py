f4b23c781c7c41fe3b7bc48bd493f1e9ce324d96
Rediet <red8alx@gmail.com>
2024-07-24 17:35:06 +0300

    programming_paradigm

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,7 @@
 class BankAccount:
-    def __init__(self, account_balance):
-        self.account_balance = account_balance
+    def __init__(self, initial_balance):
+        self.account_balance = 0
+        self.account_balance += initial_balance
     def deposit(self, amount):
         self.account_balance += amount
     def withdraw(self, amount):
