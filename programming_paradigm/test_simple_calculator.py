61e40d7e56e168779e3d28c6ee837b552f4f05f0
Rediet <red8alx@gmail.com>
2024-07-24 19:27:02 +0300

    programming_paradigm

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,25 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self): #What does this method does??
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        #self.assertEqual(self.calc.add(-1, 1), 0)
+        # Add more assertions to thoroughly test the add method.
+    def test_subtract(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.subtract(2, 3), -1)
+        #self.assertEqual(self.calc.asubtract(-1, 1), 0)
+    def test_multiply(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        #self.assertEqual(self.calc.multiply(-1, 1), 0)
+    def test_divide(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.divide(12, 3), 4)
+        #self.assertEqual(self.calc.divide(-1, 1), 0)
+        self.assertRaises(ZeroDivisionError, self.calc.divide, 10, 0)
