df7e16b391a0ff08ba375dbc52c18e280012691a
Rediet <red8alx@gmail.com>
2024-07-25 18:58:54 +0300

    oop

--- oop/library_system.py
+++ oop/library_system.py
@@ -25,15 +25,3 @@ class Library:
     def list_books(self):
         for i in self.books:
             print(i)
-
-my_library = Library()
-
-classic_book = Book("Pride and Prejudice", "Jane Austen")
-digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
-paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
-
-my_library.add_book(classic_book)
-my_library.add_book(digital_novel)
-my_library.add_book(paper_novel)
-
-my_library.list_books()
